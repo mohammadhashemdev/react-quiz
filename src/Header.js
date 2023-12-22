b3a44a824e62517d4fc334a5dddea01229ae7dff
Mohammad Hashem <hashimhanify@gmail.com>
2023-12-22 17:12:26 +0430

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>
+        - The <span>React</span> Quiz -
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
