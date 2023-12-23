fb65443c64316eebd9b5b1deebf1f5651b4444af
Mohammad Hashem <hashimhanify@gmail.com>
2023-12-23 14:34:32 +0430

    loading, error, and fake API added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+
+  // loding, ready, active, finished, error
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("unknown action");
+  }
+}
+
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  const numQuestions = questions.length;
   return (
     <div className="App">
       <Header />
       <Main>
-        <p>salam</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
--- src/Error.js
+++ src/Error.js
@@ -1,9 +1,5 @@
 function Error() {
-  return (
-    <p className="error">
-      <span>💥</span> There was an error fecthing questions.
-    </p>
-  );
+  return <p className="error">There was an error fecthing questions.</p>;
 }
 
 export default Error;
